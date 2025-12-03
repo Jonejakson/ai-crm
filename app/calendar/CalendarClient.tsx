8e1531c9d9e71848f144f70e221eff633b4b448a
Jonejakson <Jonejakson@users.noreply.github.com>
2025-12-03 23:01:54 +0300

    обавил недостающий импорт CalendarIcon, EditIcon и EmptyIcon в calendar/CalendarClient.tsx

--- app/calendar/CalendarClient.tsx
+++ app/calendar/CalendarClient.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import toast from 'react-hot-toast'
 import UserFilter from '@/components/UserFilter'
 import ExportButton from '@/components/ExportButton'
+import { CalendarIcon, EditIcon, EmptyIcon } from '@/components/Icons'
 
 interface Event {
   id: number
